ea1be89c6cd101e4dc3050f4c710c3bb57f995d2
taphead <techne.trax@proton.me>
2024-03-02 19:58:06 -0500

    first commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                {/* USE ANON ARROW FUNCTION TO PASS ARGUMENTS TO oSS */}
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
